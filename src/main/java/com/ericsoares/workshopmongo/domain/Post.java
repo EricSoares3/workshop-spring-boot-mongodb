8a0316924e0e01de8bf7226b6ac0ad16b6f3dca4
EricSoares3 <contato.eric.sm@gmail.com>
2018-08-11 17:22:11 -0300

    Acrescentado comentarios a os posts

--- src/main/java/com/ericsoares/workshopmongo/domain/Post.java
+++ src/main/java/com/ericsoares/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.ericsoares.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.ericsoares.workshopmongo.dto.AuthorDTO;
+import com.ericsoares.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{	
@@ -18,6 +21,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -31,6 +36,8 @@ public class Post implements Serializable{
 		this.title = title;
 		this.body = body;
 		this.author = author;
+		
+		
 	}
 
 	public String getId() {
@@ -75,6 +82,17 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -100,6 +118,9 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
+
+
+
 
 
 	
